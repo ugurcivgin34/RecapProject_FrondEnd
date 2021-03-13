31b8244f6c372766088768250e99b3d20f6c2ecf
okan çivgin <okanbjkspd@hotmail.com>
2021-03-13 15:55:06 +0300

    asd

---
+++ src/app/models/rental.ts
@@ -0,0 +1,11 @@
+export interface Rental {
+    rentalId:number;
+    descripton:string;
+    companyName:string;
+    brandName:string;
+    firstName:string;
+    lastName:string;
+    rentDate:Date;
+    returnDate:Date;
+    dailyPrice:number;
+}
